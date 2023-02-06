f90b5dfbacb0fbdc9f0cdd50eade2aaee6e1346d
Jeferson Bernardino <jefersondsb@hotmail.com>
2023-02-06 16:46:19 -0300

    Aulas de variaveis

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# Aulas de variaveis.
+valor1_str = input('digite o primeiro valor: ')
+print("valor1: " + valor1_str)
+valor2_str = input('digite o segundo valor: ')
+print("valor2: " + valor2_str)
+
+soma= valor1_str + valor2_str
+print("resultado da str: " + soma)
+
+valor1_int = int(valor1_str)
+valor2_int = int(valor2_str)
+soma = valor1_int + valor2_int
+print("resultado do int: " + str(soma))
+
+subtracao = valor1_int - valor2_int
+print("resultado do int: " + str(subtracao))
