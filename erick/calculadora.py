1c624c9ef9d3140271ab24bc0f5c9b5010ce26c0
Jeferson Bernardino <jefersondsb@hotmail.com>
2023-02-08 15:25:38 -0300

    acrescentando comentararios nas linhas

--- erick/calculadora.py
+++ erick/calculadora.py
@@ -1,16 +1,18 @@
 # Aulas de variaveis.
-valor1_str = input('digite o primeiro valor: ')
-print("valor1: " + valor1_str)
-valor2_str = input('digite o segundo valor: ')
-print("valor2: " + valor2_str)
+valor1_str = input('digite o primeiro valor: ') # pegando valor digitado na entrada.
+print("valor1: " + valor1_str) # imprime o valor que o usuario  digitou.
+valor2_str = input('digite o segundo valor: ') # pegando valor digitado na entrada
+print("valor2: " + valor2_str) # imprime o valor que o usuario  digitou
 
-soma= valor1_str + valor2_str
-print("resultado da str: " + soma)
+soma= valor1_str + valor2_str # soma os dois valores.
+print("resultado da str: " + soma) # imprime a soma dos valores.
 
-valor1_int = int(valor1_str)
-valor2_int = int(valor2_str)
-soma = valor1_int + valor2_int
-print("resultado do int: " + str(soma))
+valor1_int = int(valor1_str) # transformando 'string' em numero inteiro. porque de forma matematica não da para soma 2 string. string = letras
+valor2_int = int(valor2_str) # transformando 'string' em numero inteiro. porque de forma matematica não da para soma 2 string. string = letras
+soma = valor1_int + valor2_int # somando dois números inteiros.
+soma_str = str(soma) # Tranformar inteiro em string
+print("resultado do int: " + soma_str) # imprime o resultado.
 
+# LICAO DE CASA, SEPARAR A LINHA 18 E COMENTAR O QUE FOI FEITO
 subtracao = valor1_int - valor2_int
 print("resultado do int: " + str(subtracao))
